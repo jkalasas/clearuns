a7984dacf02765e29a176112b810a6b208366b62
jkalasas <alasasjohnkyle@gmail.com>
2024-02-24 22:59:07 +0800

    bcrypt password

--- src/includes/db/auth.php
+++ src/includes/db/auth.php
@@ -3,14 +3,14 @@ include_once __DIR__ . "/models/user.php";
 
 function authenticateUser(PDO $conn, string $email, string $password): ?User
 {
-	$stmt = $conn->prepare("SELECT * FROM users WHERE email=? AND password=?");
-	$stmt->bindParam(1, $email);
-	$stmt->bindParam(2, $password);
+	$stmt = $conn->prepare("SELECT * FROM users WHERE email=?");
+	$stmt->bindParam(1, trim($email));
 	$stmt->execute();
 
 	$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
 	if ($user_data == null) return null;
+	else if (!password_verify(trim($password), $user_data["password"])) return null;
 
 	return new User($user_data["id"], $user_data["email"], $user_data["password"]);
 }
