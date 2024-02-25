9a4e84a22c89178747c74e18b5cf0a5ceb42a944
jkalasas <alasasjohnkyle@gmail.com>
2024-02-25 20:44:10 +0800

    trimmed email

--- src/includes/db/auth.php
+++ src/includes/db/auth.php
@@ -3,8 +3,9 @@ include_once __DIR__ . "/models/user.php";
 
 function authenticateUser(PDO $conn, string $email, string $password): ?User
 {
+	$email = trim($email);
 	$stmt = $conn->prepare("SELECT * FROM users WHERE email=?");
-	$stmt->bindParam(1, trim($email));
+	$stmt->bindParam(1, $email);
 	$stmt->execute();
 
 	$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
