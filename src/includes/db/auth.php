e2a1480fa1df4633825c5cc755f672d67e702891
jkalasas <alasasjohnkyle@gmail.com>
2024-02-25 21:17:32 +0800

    added additional fields to User class

--- src/includes/db/auth.php
+++ src/includes/db/auth.php
@@ -13,7 +13,17 @@ function authenticateUser(PDO $conn, string $email, string $password): ?User
 	if ($user_data == null) return null;
 	else if (!password_verify(trim($password), $user_data["password"])) return null;
 
-	return new User($user_data["id"], $user_data["email"], $user_data["password"]);
+	return new User(
+		$user_data["id"],
+		$user_data["email"],
+		$user_data["password"],
+		$user_data["first_name"],
+		$user_data["last_name"],
+		$user_data["created_at"],
+		$user_data["updated_at"],
+		$user_data["middle_initial"],
+		$user_data["suffix"],
+	);
 }
 
 function getAuthUser(PDO $conn, int $userID): ?User
@@ -25,5 +35,15 @@ function getAuthUser(PDO $conn, int $userID): ?User
 	$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 	if ($user_data == null) return null;
 
-	return new User($user_data["id"], $user_data["email"], $user_data["password"]);
+	return new User(
+		$user_data["id"],
+		$user_data["email"],
+		$user_data["password"],
+		$user_data["first_name"],
+		$user_data["last_name"],
+		$user_data["created_at"],
+		$user_data["updated_at"],
+		$user_data["middle_initial"],
+		$user_data["suffix"],
+	);
 }
--- src/includes/db/models/user.php
+++ src/includes/db/models/user.php
@@ -4,11 +4,39 @@ class User
 	public int $id;
 	public string $email;
 	public string $password;
+	public string $first_name;
+	public string $last_name;
+	public string|null $middle_initial = null;
+	public string|null $suffix = null;
+	public DateTime $created_at;
+	public DateTime $updated_at;
 
-	public function __construct(int $id, string $email, string $password)
-	{
+
+	public function __construct(
+		int $id,
+		string $email,
+		string $password,
+		string $first_name,
+		string $last_name,
+		string|DateTime $created_at,
+		string|DateTime $updated_at,
+		string|null $middle_initial = null,
+		string|null $suffix = null,
+	) {
 		$this->id = $id;
 		$this->email = $email;
 		$this->password = $password;
+		$this->first_name = $first_name;
+		$this->last_name = $last_name;
+		$this->middle_initial = $middle_initial;
+		$this->suffix = $suffix;
+
+		if (gettype($created_at) == "string")
+			$this->created_at = new DateTime($created_at);
+		else $this->created_at = $created_at;
+
+		if (gettype($updated_at) == "string")
+			$this->updated_at = new DateTime($updated_at);
+		else $this->updated_at = $updated_at;
 	}
 }
